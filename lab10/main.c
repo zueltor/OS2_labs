315f29ecc95a40de2cc354dac48b0c51da6f8369
zueltor <st34l@mail.ru>
2020-10-22 14:46:37 +0700

    labs

---
+++ lab10/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define LINES_COUNT 10
+#define PARENT 1
+#define CHILD 0
+
+typedef struct {
+    char *thread_name;
+    bool can_print;
+    bool cannot_print;
+    pthread_mutex_t *permission_lock;
+} Thread_parameters;
+
+void printError(char *text, int error) {
+    if (NULL == text) {
+        return;
+    }
+    fprintf(stderr, "%s: %s\n", text, strerror(error));
+}
+
+void *parentPrintLines(void *args) {
+    if (NULL == args) {
+        return NULL;
+    }
+    Thread_parameters *parameters = (Thread_parameters *) args;
+    char *name = parameters->thread_name;
+    int can_print = parameters->can_print;
+    int cannot_print = parameters->cannot_print;
+    pthread_mutex_t *permission_lock = parameters->permission_lock;
+    bool permission = true;
+
+    int i = 1;
+    while (i <= LINES_COUNT) {
+        pthread_mutex_lock(permission_lock);
+        if (permission == can_print) {
+            printf("%s thread: line %d\n", name, i);
+            i++;
+            permission = cannot_print;
+        }
+        pthread_mutex_unlock(permission_lock);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+    Thread_parameters parameters[2];
+    pthread_mutex_t permission_lock = PTHREAD_MUTEX_INITIALIZER;
+    parameters[CHILD].can_print = false;
+    parameters[CHILD].cannot_print = true;
+    parameters[CHILD].thread_name = "Child";
+    parameters[CHILD].permission_lock = &permission_lock;
+    parameters[PARENT].can_print = true;
+    parameters[PARENT].cannot_print = false;
+    parameters[PARENT].thread_name = "Parent";
+    parameters[PARENT].permission_lock = &permission_lock;
+
+    int error = pthread_create(&thread, NULL, parentPrintLines, &parameters[CHILD]);
+    if (error) {
+        printError("Could not create thread", error);
+        return EXIT_FAILURE;
+    }
+    parentPrintLines(&parameters[PARENT]);
+    pthread_exit(NULL);
+    return EXIT_SUCCESS;
+}
